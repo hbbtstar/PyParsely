856d07d7a0558f547ebcb6a98f84b794a374515f
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-11-03 03:18:46 -0500

    override post __getattr__ to not hammer API

--- lib/api_objects.py
+++ lib/api_objects.py
@@ -5,15 +5,37 @@ class Post(object):
     def __unicode__(self):
         return self.title + "({})".format(self.url)
         
-    def __init__(self, client, post_url):
+    def __repr__(self):
+        return "Post('{}')".format(self.url)
+        
+    def __init__(self, client, post_url, access=False):
         self.url = post_url
         self._client = client
-        post_resp = self._client.get('analytics', 'post', 'detail', url=post_url)
+        if access:
+            self.access_API()
+            self.accessed = True
+        else:
+            self.accessed = False
+        
+    # only access the API if the post ends up being used- this lets people
+    # work with URLs of posts without having to query the API
+    def __getattr__(self, item):
+        if self.accessed or item == "url":
+            return object.__getattribute__(self, item)
+        elif self.accessed == False:
+            self.access_API()
+            return object.__getattribute__(self, item)
+        else:
+            return object.__getattr__(self, item)
+        
+    def access_API(self):
+        post_resp = self._client.get('analytics', 'post', 'detail', url=self.url)
         for key, result in post_resp['data'][0].items():
             setattr(self, key, result)
         self.hits = self._hits
         self.thumbnail = self.thumb_url_medium
         self.image = self.image_url
+        self.accessed = True
         
         
         
